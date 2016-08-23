68fd47e0d1ee5bc1d58bb956353a991e3c508903
Leonard Martin <leonard.martin@gmail.com>
2016-08-23 16:11:05 +0100

    First implementation of climate data provider

---
+++ index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Promise = require('bluebird');
+const sauce = require('mintsauce');
+const lambda = sauce();
+
+const lookup = require('./lib/lookup-climate');
+
+// setup sns helpers
+lambda.use(sauce.middleware.SNS);
+
+// create common parameters used to send results to client
+lambda.use((call, response, next) => {
+  response.params = {
+    id: call.body.context.connectionId,
+    searchId: call.body.context.searchId,
+    userId: 1,
+    provider: call.name
+  };
+  next();
+});
+
+// get the destination tiles from the inbound message
+lambda.use((call, response, next) => {
+  call.body.destinations = call.body.content.tiles.filter((tile) => {
+    return tile.match(/^tile:destination\./);
+  });
+  next();
+});
+
+// iterate over the destination tiles, perform a weather lookup
+// for each and send the results to the client
+lambda.use((call, response, next) => {
+  return Promise.map(call.body.destinations, (destination) => {
+    return lookup(destination)
+      .then((data) => {
+        if (!data) return;
+        // send the result to the client via sns
+        const output = Object.assign({ items: [ data ] }, response.params);
+        return Promise.promisify(response.sns.send)('search-results-v1-ci', output).then(() => data);
+      });
+  }, { concurrency: 3 })
+  .then((results) => {
+    response.data = results.filter(o => o);
+  });
+});
+
+// emit a complate event to the client
+lambda.use((call, response, next) => {
+  const output = Object.assign({ searchComplete: true }, response.params);
+  response.sns.send('search-results-v1-ci', output, (e) => next(e));
+});
+
+// send callback
+lambda.use((call, response) => {
+  response.send(response.data);
+});
+
+module.exports = {
+  handler: lambda
+};
